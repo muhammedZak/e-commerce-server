2b9e4658addb5ad055a09efc071295d9507bf27e
Muhammed Zakariya <zakariya.zaku018@gmail.com>
2026-02-17 20:01:38 +0530

    add User model schema with role-based access

---
+++ models/userModel.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: [8, 'Password must be at least 3 characters long'],
+      maxLength: [12, 'Password cannot exceed 20 characters'],
+    },
+    role: {
+      type: String,
+      enum: ['customer', 'admin'],
+      default: 'customer',
+    },
+    phone: {
+      type: String,
+    },
+
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+module.exports = mongoose.model('User', userSchema);
